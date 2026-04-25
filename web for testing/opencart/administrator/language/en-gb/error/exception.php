b2fec90ba7f5ff3a5c62ae1c15d2733cbcd57d94
Quang, NGUYEN DANG <ndquang4@cmcglobal.vn>
2026-04-25 23:20:57 +0700

    handle response for exception and permission

---
+++ web for testing/opencart/administrator/language/en-gb/error/exception.php
@@ -0,0 +1,6 @@
+<?php
+// Heading
+$_['heading_title']    = 'Error!';
+
+// Text
+$_['text_exception']   = 'An error occurred: %s';
---
+++ web for testing/opencart/administrator/language/vi-vn/error/exception.php
@@ -0,0 +1,6 @@
+<?php
+// Heading
+$_['heading_title']    = 'Lỗi!';
+
+// Text
+$_['text_exception']   = 'Đã xảy ra lỗi: %s';
